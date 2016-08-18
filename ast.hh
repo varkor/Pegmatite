76ea6a1ec2f2578062b8c60282ff0bde1877f307
David Chisnall <theraven@theravensnest.org>
2016-08-18 13:21:17 +0100

    Bump copyright.

--- ast.hh
+++ ast.hh
@@ -1,6 +1,6 @@
 /*-
  * Copyright (c) 2012, Achilleas Margaritis
- * Copyright (c) 2014, David T. Chisnall
+ * Copyright (c) 2014-2016, David T. Chisnall
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
