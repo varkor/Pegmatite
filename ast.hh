0b4d9477f12f4a38e5818983a85c1d0e473d506d
Jonathan Anderson <jonathan.anderson@ieee.org>
2016-08-19 10:49:23 +0100

    Fix size_t format string.

--- ast.hh
+++ ast.hh
@@ -50,7 +50,7 @@ std::string demangle(std::string);
 template <class T> void debug_log(const char *msg, size_t depth, T *obj)
 {
 	std::string demangled = demangle(typeid(*obj).name());
-	fprintf(stderr, "[%ld] %s %s (%p) off the AST stack\n",
+	fprintf(stderr, "[%zd] %s %s (%p) off the AST stack\n",
 			depth, msg, demangled.c_str(), static_cast<const void*>(obj));
 }
 #else
